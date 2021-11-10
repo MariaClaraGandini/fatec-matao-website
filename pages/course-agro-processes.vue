a7fbbd7f7982199e7c9de12cbbe8edc77c6b506c
Mariana Lourenço <79528709+MarianasdLourenco@users.noreply.github.com>
2021-11-10 09:36:03 -0300

    Update course-agro-processes.vue

--- pages/course-agro-processes.vue
+++ pages/course-agro-processes.vue
@@ -1,7 +1,146 @@
 <template>
+  <main>
+    <b-card class="form-card">
+      <b-card-text class="curses-style"> Análise de Processos Agroindustriais </b-card-text>
+    </b-card>
 
+    <b-container class="py-4">
+      <b-row class="text-center">
+        <b-col cols="12">
+          <h2 class="subtitle-style">Áreas de Atuação</h2>
+
+          <hr class="hr-curses mt-3" />
+
+          <p class="description">
+           O Tecnólogo em Análise de Processos Agroindustriais, desenvolve
+            processos agroindustriais, de trabalho e fabricação.
+            aborda matérias fundamentais como gestão, automação, produção
+            industrial, informática e ciências exatas. <br />
+            O aluno vai estar apto para redesenhar e inovar processos, aprender
+            conceito de fábricas inteligentes e manufatura avançada 
+            também pode trabalhar juntando a automação, produção e informática. <br />
+            - Duração: 6 semestres. <br />
+            - Período: matutino e noturno.<br />
+            - Vagas: 40 vagas por período.
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container class="py-0">
+      <b-row class="text-center">
+        <b-col cols="12">
+          <h2 class="subtitle-style">Perfil Do Profissional</h2>
+
+          <hr class="hr-curses mt-3" />
+
+          <p class="description">
+            O profissional de Análise de Processos Agroindustriais pode atuar em
+            empresas de gestão da produção industrial, automação industrial, e
+            banco de dados, assim como empreender abrindo um comércio.
+          </p>
+
+          <div class="mt-3">
+            <b-card
+              img-src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083390_960_720.jpg"
+              img-alt="Card image"
+              img-left
+              class="mb-0"
+              style="width: 200px; height: 180px"
+            >
+              <b-card-text>
+                <p class="description-class">
+                  <b>Coordenador do curso </b><br />
+                  Prof. usuario do curso <br />
+                  <b>Email do coordenador </b><br />
+                  usuariodocurso@fatec.sp.gov.br
+                </p>
+                <a href="#" target="_blank" class="link-one"> Grade </a>
+                <a href="#" target="_blank" class="link-one"> horário </a>
+              </b-card-text>
+            </b-card>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </main>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  head() {
+    return {
+      title: "Fatec Matão | Cursos",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'O Tecnólogo em Alimentos, desenvolve tecnologia para diversos produtos!'
+        },
+        // Open Graph
+        { property: 'og:title', content: 'Fatec Matão | Cursos' },
+        { property: 'og:site_name', content: 'Fatec Matão | Cursos' },
+        { property: 'og:description', content: 'O Tecnólogo em Alimentos, desenvolve tecnologia para diversos produtos!' }
+      ]
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
+  },
+};
+</script>
 
+<style scoped>
+p.description {
+  color: var(--black-200-color);
+  font-size: 1.1rem;
+  text-align: justify;
+}
+p.description span {
+  color: var(--secondary-color);
+  font-weight: 500;
+  text-align: justify;
+}
+.description-class {
+  font-weight: 500;
+  text-align: justify;
+  color: var(--black-200-color);
+  font-size: 1.2rem;
+  margin-top: -18px;
+}
+.form-card {
+  background: var(--secondary-color);
+  font-weight: 10;
+}
+.curses-style {
+  color: var(--white-color);
+  font-size: 2.5rem;
+  font-weight: 300px;
+  text-align: center;
+}
+.hr-curses {
+  background-color: var(--black-200-color);
+  border: 2px solid var(--black-200-color);
+  width: 300px;
+}
+a.link-one {
+  background-color: var(--primary-color);
+  border-radius: 5px;
+  border: 2px solid var(--primary-color);
+  color: var(--white-color);
+  font-weight: 500;
+  margin-right: 25px;
+  padding: 5px 35px;
+}
 </style>
